cdcdf4e5c10a646ee4a768025c8e9ba14d76bbbb
priyanshu kumar <dlnpriyanshu@gmail.com>
2023-12-13 06:39:00 +0530

    added

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from '../src/components/navbar.js';
+import Footer from './components/footer.js';
+import Homepage from './components/homepage.js';
+function App() {
+  return (
+  <>
+    <Navbar />
+    <Homepage />
+    <Footer />
+  </>
+  );
+}
+
+export default App;
