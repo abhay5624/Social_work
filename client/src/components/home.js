d1edf80763b055c877195a109e7080cf1d567b9e
priyanshu kumar <dlnpriyanshu@gmail.com>
2023-12-14 10:16:00 +0530

    forms

---
+++ client/src/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './navbar.js';
+import Footer from './footer.js';
+import Homepage from './homepage.js';
+export default function home() {
+  return (
+    <>
+    <Navbar />
+    <Homepage />
+    <Footer />
+    
+    </>
+  )
+}
