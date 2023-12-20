7561289bb89eccfabe1268c651970427165c2cf0
priyanshu kumar <dlnpriyanshu@gmail.com>
2023-12-20 20:51:10 +0530

    so many changes

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -5,25 +5,37 @@ import styled from 'styled-components';
 import Loading from "../Assets/Image/Loading.png"
 import Post from '../components/Post';
 import { useNavigate } from 'react-router-dom';
+import '../css/pagecss/profile.css'
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
 const Profile = () => {
     const {Data} = useLocal();
     const {userProfile} = useLocal();
     const Navigate = useNavigate();
   return (
-    <ProfileDiv>
+
+    <>
+    <Navbar />
+    
+    <div className='ProfileDiv'>
       {
         (!Data.firstName)? <div className="loader">
           <img src={Loading} alt="" />  
         </div>:
       <>
         <div className="backdroundDiv">
+          
         {(userProfile)? <img src={userProfile.background} alt="" /> : <img src={Loading} alt="" />}
           <div className="avatar"><img src={(userProfile)? userProfile.avatar: Avatar} alt="We need your profile" /></div>
         </div>
+        <br/>
         <div className="nameDetail">
           <div className="flexbox">
+          
+
           <h2>{Data.firstName}</h2>
-          <button onClick={() => {Navigate('/updateProfile')}}>Edit Profile</button>
+          <button onClick={() => {Navigate('/updateProfile')}}>🖋️</button>
+         
           </div>
           <p style={{fontWeight: 600,fontSize: "20px"}}>{Data.email.split("@nitjsr.ac.in")}</p>
           <p style={{fontWeight: 500}}>{userProfile ? userProfile.headLine: ""}
@@ -31,63 +43,65 @@ const Profile = () => {
           <p>{userProfile? userProfile.discription: ""}</p>
           <p>{Data.email}</p>
         </div>
-        ()
+        
         <Post />
+        
         </>
       }
-    </ProfileDiv>
+    </div>
+      </>
   )
 }
-const ProfileDiv = styled.div`
-.loader{
-  width: 100vw;
-  height: 100vh;
-  background: #ddd;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.flexbox{
-  display: flex;
-  justify-content: space-between;
-  button {
-    padding: 10px 30px;
-    color: white;
-    background: #190582;
-    border: none;
-    border-radius: 5px;
-    font-size: 15px;
-  }
-}
-.backdroundDiv {
-width: 100%;
-height:300px;
-background: black;
-position: relative;
-img{
-  width: 100%;
-  height: 100%;
-}
-.avatar{
-  border: 5px solid white;
-  position: absolute;
-  bottom: -60%;
-  left: 100px;
-  width: 300px;
-  height: 300px;
-  border-radius: 50%;
-  background: pink;
-  img{
-    width: 299px;
-    height: 299px;
-    border-radius: 50%;
-  }
-}
-}
-.nameDetail{
-  padding: 30px;
-  width: 50%;
-  margin-left: 450px;
-}
-`
+// const ProfileDiv = styled.div`
+// .loader{
+//   width: 100vw;
+//   height: 100vh;
+//   background: #ddd;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// }
+// .flexbox{
+//   display: flex;
+//   justify-content: space-between;
+//   button {
+//     padding: 10px 30px;
+//     color: white;
+//     background: #190582;
+//     border: none;
+//     border-radius: 5px;
+//     font-size: 15px;
+//   }
+// }
+// .backdroundDiv {
+// width: 100%;
+// height:300px;
+// background: black;
+// position: relative;
+// img{
+//   width: 100%;
+//   height: 100%;
+// }
+// .avatar{
+//   border: 5px solid white;
+//   position: absolute;
+//   bottom: -60%;
+//   left: 100px;
+//   width: 300px;
+//   height: 300px;
+//   border-radius: 50%;
+//   background: pink;
+//   img{
+//     width: 299px;
+//     height: 299px;
+//     border-radius: 50%;
+//   }
+// }
+// }
+// .nameDetail{
+//   padding: 30px;
+//   width: 50%;
+//   margin-left: 450px;
+// }
+// `
 export default Profile
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,58 +2,44 @@ import React from 'react'
 import { useLocal } from '../store/auth_context'
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import '../css/componentcss/sidebar.css'
 const Sidebar = () => {
     const {Data} = useLocal();
     const {userProfile} = useLocal();
   return (
-    <AsideBar>
+    <>
+   
+
+    <input type="checkbox" id="tg" />
+   <label htmlFor="tg" id="profile-toggler">
+     <div id="popen">🔘</div>
+     <div id="pclose">✖️</div>
+   </label>
+    <div id='sideBar'>
+       
+    
     {userProfile? <img src={userProfile.avatar} style={{borderRadius: "50%"}} alt="" />: ""}    
+    
     {Data? <h4>{Data.firstName}</h4>:""}
    
     
-    <div className="line"></div>
-    {userProfile?     <h5 style={{gridColumn: "span 2",textAlign: "center"}}>{userProfile.headLine}</h5>
+    {/* <div className="line"></div> */}
+   
+    {userProfile?     <h5>{userProfile.headLine}</h5>
 :""}
-    <Link style={{gridColumn: "span 2"}} to="../profile">My Profile</Link>
-    <Link style={{gridColumn: "span 2"}} to="../updateProfile">Edit my Profile</Link>
-    <Link style={{gridColumn: "span 2"}} to="../createPost">Create a new Post</Link>
-    <Link style={{gridColumn: "span 2"}} to="../search">Advance Search</Link>
-    <Link to = "../messages"> Message</Link>
-    <Link to = "../logout"> Log Out</Link>
-    </AsideBar>
+<br/>
+    <div className='options'><Link to="../profile">My Profile</Link></div>
+    <div className='options'><Link to="../updateProfile">Edit my Profile</Link></div>
+    <div className='options'><Link to="../createPost">Create a new Post</Link></div>
+    <div className='options'><Link to="../search">Advance Search</Link></div>
+    <div className='options'>
+        <Link to = "../messages"> Message</Link>
+        <Link to = "../logout"> Log Out</Link>
+        </div>
+    </div>
+
+</>
   )
 }
-const AsideBar = styled.div`
-display: grid;
-grid-template-columns: auto auto;
-grid-template-rows: 100px 5px 50px 50px 50px 50px 50px 50px;
-padding: 15px;
-width: 15%;
-color: white;
-height: 100vh;
-position: fixed;
-z-index: 10;
-left: 0;
-a{
-    margin: 5px;
-    padding: 3px;
-    border: 2px solid white;
-    color: white;
-    grid-columns: span 2; 
-}
-.line{
-    max-height: 3px;
-    grid-column: span 2;
-    background: white;
-}
-background-color: #190582;
-h4{
-    margin-top: 30px;
-    height: 80px;
-}
-img{
-    width: 60%;
-    margin: 20px;
-}
-`
+
 export default Sidebar
