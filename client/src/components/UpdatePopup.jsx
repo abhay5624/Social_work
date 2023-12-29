6fc37451935d2927675c7745bcb404b17aafdaf9
abhay5624 <abhaysharma7435@gmail.com>
2023-12-29 18:56:07 +0530

    update profile done

--- client/src/components/UpdatePopup.jsx
+++ client/src/components/UpdatePopup.jsx
@@ -3,16 +3,51 @@ import styled from 'styled-components'
 import { useLocal } from '../store/auth_context';
 import '../css/componentcss/updatepopup.css'
 import Loader from '../Assets/Image/loader.gif'
-
+import { useNavigate } from 'react-router-dom';
 const UpdatePopup = (Props) => {
-    const [prop,setProp] = useState({
+    const Navigate = useNavigate();
+    const [prop, setProp] = useState({
+        id: Props.data._id,
         title: Props.data.title,
         description: Props.data.description,
         tags:Props.data.tags,
         popupimg:Props.data.postImg
     });
-    // console.log("from data ",Props.data.title)
-  return (
+    const [tagArray, setTagArray] = useState(prop.tags);
+    const [tg, setTg] = useState();
+    const updateTagAdd = () => {
+        setTagArray([...tagArray, tg]);
+        setProp({ ...prop, tags: tagArray });
+    }
+    const UpdateTagsDelete = (e) => {
+        e.preventDefault();
+        const parentEl = e.target.parentElement;
+        const data = tagArray.filter((element) => {
+            return element != parentEl.firstElementChild.innerText;
+        })
+        setTagArray(data);
+    }
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+       
+        const token = localStorage.getItem("token");
+        const respond = await fetch(`http://localhost:3001/api/auth/post`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify(prop)
+        });
+        if (respond.ok) {
+            //Navigate(`../profile`);
+            console.log(await respond.json());
+        } else {
+            alert("Error arived");
+        }
+       
+    }
+   return (
     <div className='mainpopup'>
 
     <div className='UpdPop'>
@@ -31,7 +66,7 @@ const UpdatePopup = (Props) => {
                     <p>{prop.description}</p>
                     <div className='poptags' >
                         {
-                            prop.tags ?prop.tags.map((element)=> (
+                            tagArray ?tagArray.map((element)=> (
                                 <button>{element}</button>
                                 )): ""
                             }
@@ -40,7 +75,7 @@ const UpdatePopup = (Props) => {
         </div>
 
         <div className='rightpopup'>
-        <form>
+        <form onSubmit={(e)=> {onSubmitHandler(e)}}>
                     <h4>Title</h4> 
                     <input type="text" name="title" value={prop.title} onChange={(e) => {setProp({...prop, title: e.target.value})}}/>
                     <h4>Discription</h4> 
@@ -48,16 +83,16 @@ const UpdatePopup = (Props) => {
                      
                     <h4 >Post Images</h4> 
                         <input type="file" name="postImg" id="popuppostImg" accept='.jpeg,.png,.jpg'  />
-                        <h4 className="add"  >➕</h4>
-                        <input type="text" name='tags' placeholder='Enter New Tag'  />
+                        <h4 className="add" onClick={()=>{updateTagAdd()}} >➕</h4>
+                        <input type="text" name='tags' placeholder='Enter New Tag'  value={tg} onChange={(e)=>setTg(e.target.value)}/>
                         <div className='poptags' >
                         {
-                            prop.tags ?prop.tags.map((element)=> (
-                                <button style={{margin: "5px"}}>{element}<button className='poptagcross'>❌</button></button>
+                            tagArray ?tagArray.map((element)=> (
+                                <button style={{margin: "5px"}}  ><span>{element}</span><button className='poptagcross' onClick={(e)=>UpdateTagsDelete(e)}>❌</button></button>
                                 )): ""
                             }
                         </div>    
-                    <button type="submit" style={{gridColumn: "span 2"}}>Submit</button>
+                    <button type="submit" style={{gridColumn: "span 2"}} onClick={(e)=> {onSubmitHandler(e)}}>Submit</button>
                 </form>
         </div>
        
@@ -68,21 +103,4 @@ const UpdatePopup = (Props) => {
   )
 }
 
-// const UpdPop = styled.div`
-// position: fixed;
-// width: 100vw;
-// height: 100vh;
-// left: 0;
-// top: 0;
-// z-index: 0;
-// display: flex;
-// justify-content: center;
-// background: white;
-// padding: 10%;
-// .Grid{
-//     display: grid;
-//     gap: 15px;
-//     grid-template-columns: 30% 70%;
-// }
-// `;
 export default UpdatePopup
